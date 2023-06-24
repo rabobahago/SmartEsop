de6412ec148a948ba4f86bc37e0e305be8dc02c0
Abdulrasheed Ibrahim <91624127+abdoulibrahim@users.noreply.github.com>
2023-06-24 12:07:03 +0100

    Create EmployeeStockOptionPlan.sol

---
+++ EmployeeStockOptionPlan.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+contract EmployeeStockOptionPlan {
+    // Define the necessary data structures and variables
+    
+    // Implement the necessary events
+    
+    // Implement the constructor
+    
+    // Implement the functions for granting stock options
+    
+    // Implement the functions for setting the vesting schedule
+    
+    // Implement the functions for exercising options
+    
+    // Implement the functions for tracking vested and exercised options
+    
+    // Implement the necessary modifiers and access control
+    
+    // Add any additional functions or modifiers as needed
+}
