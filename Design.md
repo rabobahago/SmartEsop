f688d7f0dc23d89ae5aa4421a623263fe1820d35
rabobahago <rabobahago@gmail.com>
2023-06-25 12:41:12 +0100

    owner address, totalOptions variable, vestedOptions mapping, onlyOwner modifier, StokOptionsGranted event, constructor and grantStockOptions function implemented

--- Design.md
+++ Design.md
@@ -1,5 +1,4 @@
 Design:
-
 1. Define the requirements. What are the specific features that the smart contract should have? What are the security and access control requirements?
 
 2. Design the contract architecture. How will the different parts of the smart contract interact with each other? What data structures will be used to store information about stock options, vesting schedules, and exercised options?
@@ -10,7 +9,9 @@ Design:
 
 5. Deploy the smart contract. Deploy the smart contract to a testnet or mainnet blockchain.
 
-Additional considerations: 6. Security. The smart contract should be designed to be secure against common attacks, such as reentrancy, overflow/underflow, and unauthorized access.
+Additional considerations:
+
+6. Security. The smart contract should be designed to be secure against common attacks, such as reentrancy, overflow/underflow, and unauthorized access.
 
 7. Scalability. The smart contract should be designed to be scalable so that it can handle a large number of transactions.
 
--- EmployeeStockOptionPlan.sol
+++ EmployeeStockOptionPlan.sol
@@ -1,21 +1,53 @@
 pragma solidity ^0.8.0;
 
 contract EmployeeStockOptionPlan {
-    // Define the necessary data structures and variables
-    
-    // Implement the necessary events
-    
-    // Implement the constructor
-    
-    // Implement the functions for granting stock options
-    
+    // Define the necessary data structures and variables::
+
+    address public owner; // Address of the contract owner
+    uint256 public totalOptions; // Total number of available options
+    mapping(address => uint256) public vestedOptions; // Mapping to track vested and exercised options for each employee
+
+    // Implement the necessary events::
+
+    // Event emitted when stock options are granted to an employee
+    event StockOptionsGranted(address indexed employee, uint256 options);
+
+    // Modifier to restrict function access to the contract owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    // Implement the constructor::
+
+    constructor() {
+        // Set the contract owner as the deployer of the contract
+        owner = msg.sender;
+    }
+
+    // Implement the functions for granting stock options::
+
+    function grantStockOptions(
+        address employee,
+        uint256 options
+    ) external onlyOwner {
+        require(options > 0, "Number of options must be greater than zero.");
+
+        vestedOptions[employee] += options; // Increase the vested options for the employee
+        totalOptions += options; // Increase the total options available
+
+        emit StockOptionsGranted(employee, options); // Emit the StockOptionsGranted event
+    }
+
+    // Function to grant stock options to an employee
+
     // Implement the functions for setting the vesting schedule
-    
+
     // Implement the functions for exercising options
-    
+
     // Implement the functions for tracking vested and exercised options
-    
+
     // Implement the necessary modifiers and access control
-    
+
     // Add any additional functions or modifiers as needed
 }
